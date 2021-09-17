cb6c6d3daa6691f484f7e6c88a4968f77d9667f4
MrYapetzku <72166768+MrYapetzku@users.noreply.github.com>
2021-09-17 20:52:26 +0300

    Init

---
+++ Assets/Scripts/GameAnimations.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameAnimations : MonoBehaviour
+{
+    [SerializeField] private UIPopup _popup;
+    [SerializeField] private BallMover _ballMover;
+
+    [SerializeField] private float _countMassageTime;
+
+    private void OnEnable()
+    {
+        _ballMover.enabled = false;
+        StartCoroutine(ShowStartAnimation());
+    }
+
+    private IEnumerator ShowStartAnimation()
+    {
+        yield return new WaitForSeconds(_countMassageTime);
+        for (int i = 3; i > 0; i--)        {
+            _popup.ShowMassage(i.ToString());
+            yield return new WaitForSeconds(_countMassageTime);
+        }
+        _popup.ShowMassage("Gooooo");
+        _ballMover.enabled = true;
+    }
+
+}
