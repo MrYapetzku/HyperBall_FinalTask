1db52d6e870f856f706be0cc6338731d701449fa
MrYapetzku <72166768+MrYapetzku@users.noreply.github.com>
2021-09-27 12:30:30 +0300

    Project refactoring

---
+++ Assets/Scripts/GameMechanics/GameStatesSwitcher.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CollisionHandler),typeof(Animator),typeof(Rigidbody))]
+public class GameStatesSwitcher : MonoBehaviour
+{
+    [SerializeField] private WaveGenerator _spawner;
+    [SerializeField] private GameObject _notInteractiveObjectsContainer;
+    [SerializeField] private GameObject _interactiveObjectsContainer;
+    [SerializeField] private UIGameMenu _gameMenu;
+
+    private Rigidbody _rigidbody;
+    private CollisionHandler _collisionHandler;
+    private Animator _animator;
+    private Vector3 _ballStartPosition;
+    private Vector3 _cameraStartPosition;
+
+    private void Awake()
+    {
+        _ballStartPosition = transform.position;
+        _cameraStartPosition = Camera.main.transform.position;
+        _rigidbody = GetComponent<Rigidbody>();
+        _collisionHandler = GetComponent<CollisionHandler>();
+        _animator = GetComponent<Animator>();
+        transform.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        _collisionHandler.GameEnding += OnGameEnding;
+    }
+
+    private void OnDisable()
+    {
+        _collisionHandler.GameEnding -= OnGameEnding;
+    }
+
+
+    public void OnStartingState()
+    {
+        Time.timeScale = 1;
+        _rigidbody.isKinematic = false;
+        _rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        SetStartPositions();
+        _animator.SetTrigger(BallAnimator.StartGame);
+        _spawner.gameObject.SetActive(true);
+        gameObject.SetActive(true);
+    }
+
+    private void OnGameOver()
+    {
+        _spawner.gameObject.SetActive(false);
+        gameObject.SetActive(false);
+        DisableObjectsInContainer(_notInteractiveObjectsContainer);
+        DisableObjectsInContainer(_interactiveObjectsContainer);
+        _gameMenu.SwitchToGameOverMenu();
+    }
+
+    private void OnGameEnding()
+    {
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+        _animator.SetTrigger(BallAnimator.GameOver);
+    }
+
+    private void SetStartPositions()
+    {
+        transform.position = _ballStartPosition;
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+        Camera.main.transform.position = _cameraStartPosition;
+    }
+
+    private void DisableObjectsInContainer(GameObject conteiner)
+    {
+        foreach (Transform item in conteiner.transform)
+            item.gameObject.SetActive(false);
+    }
+}
